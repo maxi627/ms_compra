ac58c13efaf2563c1fc56d9f4d0f8b36ccf47f9e
maxi627 <eulamaxi@gmail.com>
2024-12-11 12:50:14 -0300

    ...

--- app/models/compra.py
+++ app/models/compra.py
@@ -7,7 +7,7 @@ class Compra(db.Model):
     __tablename__ = 'Compra'
 
     id: int = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
-    producto_id: int = db.Column('producto_id', db.Integer, db.ForeignKey('producto.id'), nullable=True)
+    producto_id: int = db.Column('producto_id', db.Integer, nullable=False)
     fecha_compra: DateTime = db.Column('fecha_compra', db.DateTime, nullable=False)
     direccion_envio: str = db.Column('direccion_envio', db.Text, nullable=False)
 
--- app/routes/compra_resource.py
+++ app/routes/compra_resource.py
@@ -1,52 +1,81 @@
-from flask import Blueprint, jsonify, request
-from app.mapping import CompraSchema
-from app.services import CompraService
+from flask import Blueprint, request
+from marshmallow import ValidationError
+
+from app.mapping import CompraSchema, ResponseSchema
+from app.services import CompraService, ResponseBuilder
 
 compra = Blueprint('compra', __name__)
 service = CompraService()
-compra_schema =CompraSchema()
+compra_schema = CompraSchema()
+response_schema = ResponseSchema()
 
-"""
-Obtiene todos las compras
-"""
 @compra.route('/compras', methods=['GET'])
 def all():
-    resp = compra_schema.dump(service.get_all(), many=True) 
-    return resp, 200
+    response_builder = ResponseBuilder()
+    try:
+        data = compra_schema.dump(service.get_all(), many=True)
+        response_builder.add_message("Compras found").add_status_code(200).add_data(data)
+        return response_schema.dump(response_builder.build()), 200
+    except Exception as e:
+        response_builder.add_message("Error fetching compras").add_status_code(500).add_data(str(e))
+        return response_schema.dump(response_builder.build()), 500
 
-"""
-Obtiene una compra por id
-"""
 @compra.route('/compras/<int:id>', methods=['GET'])
 def one(id):
-    resp = compra_schema.dump(service.get_by_id(id)) 
-    return resp, 200
+    response_builder = ResponseBuilder()
+    try:
+        data = service.get_by_id(id)
+        if data:
+            serialized_data = compra_schema.dump(data)
+            response_builder.add_message("Compra found").add_status_code(200).add_data(serialized_data)
+            return response_schema.dump(response_builder.build()), 200
+        else:
+            response_builder.add_message("Compra not found").add_status_code(404).add_data({'id': id})
+            return response_schema.dump(response_builder.build()), 404
+    except Exception as e:
+        response_builder.add_message("Error fetching compra").add_status_code(500).add_data(str(e))
+        return response_schema.dump(response_builder.build()), 500
 
-"""
-Crea nueva compra
-"""
 @compra.route('/compras', methods=['POST'])
 def create():
-    compra = compra_schema.load(request.json)
-    resp = compra_schema.dump(service.create(compra))
-    return resp, 201
+    response_builder = ResponseBuilder()
+    try:
+        compra = compra_schema.load(request.json)
+        data = compra_schema.dump(service.create(compra))
+        response_builder.add_message("Compra created").add_status_code(201).add_data(data)
+        return response_schema.dump(response_builder.build()), 201
+    except ValidationError as err:
+        response_builder.add_message("Validation error").add_status_code(422).add_data(err.messages)
+        return response_schema.dump(response_builder.build()), 422
+    except Exception as e:
+        response_builder.add_message("Error creating compra").add_status_code(500).add_data(str(e))
+        return response_schema.dump(response_builder.build()), 500
 
-"""
-Actualiza una compra existente
-"""
 @compra.route('/compras/<int:id>', methods=['PUT'])
 def update(id):
-    compra = compra_schema.load(request.json)
-    resp = compra_schema.dump(service.update(id, compra))
-    return resp, 200
+    response_builder = ResponseBuilder()
+    try:
+        compra = compra_schema.load(request.json)
+        data = compra_schema.dump(service.update(id, compra))
+        response_builder.add_message("Compra updated").add_status_code(200).add_data(data)
+        return response_schema.dump(response_builder.build()), 200
+    except ValidationError as err:
+        response_builder.add_message("Validation error").add_status_code(422).add_data(err.messages)
+        return response_schema.dump(response_builder.build()), 422
+    except Exception as e:
+        response_builder.add_message("Error updating compra").add_status_code(500).add_data(str(e))
+        return response_schema.dump(response_builder.build()), 500
 
-"""
-Elimina una compra existente
-"""
 @compra.route('/compras/<int:id>', methods=['DELETE'])
 def delete(id):
-    msg = "compra eliminado correctamente"
-    resp = service.delete(id)
-    if not resp:
-        msg = "No se pudo eliminar el compra"
-    return jsonify(msg), 204
+    response_builder = ResponseBuilder()
+    try:
+        if service.delete(id):
+            response_builder.add_message("Compra deleted").add_status_code(200).add_data({'id': id})
+            return response_schema.dump(response_builder.build()), 200
+        else:
+            response_builder.add_message("Compra not found").add_status_code(404).add_data({'id': id})
+            return response_schema.dump(response_builder.build()), 404
+    except Exception as e:
+        response_builder.add_message("Error deleting compra").add_status_code(500).add_data(str(e))
+        return response_schema.dump(response_builder.build()), 500
